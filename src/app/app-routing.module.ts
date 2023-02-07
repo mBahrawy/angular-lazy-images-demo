f2ed2cceced08283bffc7f4ee3cbc015e00251f5
Mohamed A. Bahrawy <mbahrawy@tendegrees.sa>
2023-02-07 22:08:41 +0200

    config custom directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestingImgaesLazyLoadingComponent } from './core/components/testing-imgaes-lazy-loading/testing-imgaes-lazy-loading.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "testing-lazy-loading-images",
+    component: TestingImgaesLazyLoadingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestingImgaesLazyLoadingComponent } from './core/components/testing-imgaes-lazy-loading/testing-imgaes-lazy-loading.component';
+import { LazyLoadImagesDirective } from './core/directives/lazy-load-images.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestingImgaesLazyLoadingComponent,
+    LazyLoadImagesDirective
   ],
   imports: [
     BrowserModule,
